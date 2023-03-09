63b907630aa24ab62d5f0f6d0962242bb88c2ea6
“Kelsang” <kelsangrtsering@gmail.com>
2023-03-09 17:33:01 -0500

    make new changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+import Example from "./scripts/example";
+
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Hello world!");
-    // const main = document.getElementById("main")
-    // new Example(main)
+    const main = document.getElementById("main")
+    new Example(main)
 })
